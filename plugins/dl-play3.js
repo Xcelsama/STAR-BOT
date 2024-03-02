41a1f751f7bfd4a78f2d7bd08becf41b787f2063
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-02 22:00:54 +0100

    Update dl-play3.js

--- plugins/dl-play3.js
+++ plugins/dl-play3.js
@@ -19,7 +19,7 @@ const handler = async (m, {
     conn.GURUPLAY = conn.GURUPLAY ? conn.GURUPLAY : {};
     await conn.reply(m.chat, wait, m);
     const result = await searchAndDownloadMusic(text);
-    const infoText = `🔰──『 *XLICON-V2 PLAYER* 』── 🔰`;
+    const infoText = `🔰──『 *ՏͲᎪᎡ-ᎷᎠ-Ꮩ2 PLAYᎬR* 』── 🔰`;
 
 const orderedLinks = result.allLinks.map((link, index) => {
     const sectionNumber = index + 1;
