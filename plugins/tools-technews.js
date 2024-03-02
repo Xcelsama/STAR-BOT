9b29ab9bb0abf7843cabfb64e554ba3b06550b60
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-02 23:00:01 +0100

    Update tools-technews.js

--- plugins/tools-technews.js
+++ plugins/tools-technews.js
@@ -8,8 +8,8 @@ let handler = async (m, { conn }) => {
     if (!json.news) throw json;
 
     let techNews = `•───── ୨❀୧ ─────•
-    ❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Active
-    ㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: XLICON TEAM 
+    ❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Online... 
+    ㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: EXCEL
     ☞ 𝑵𝑬𝑾𝑺: ${json.news}
       •───── ୨❀୧ ─────•
     `;
