75121aefe2aee8b28bb3797c57e2120a024e0551
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-02 22:52:50 +0100

    Update maker-logo.js

--- plugins/maker-logo.js
+++ plugins/maker-logo.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* XLICON`
-	let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* XLICON *+* BOT`
+	let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* STAR *+* BOT`
 	
 	m.react(rwait);
 	
