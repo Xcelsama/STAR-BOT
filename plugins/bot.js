26c8f7ca009f8f4f53e1dd6a80218e222523efe2
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-02 15:21:35 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,11 +5,11 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3", "Xlicon4"])}.mp3`
 
-m.reply( `Hello ${taguser} Am Xlicon-V2 creatd by team Xlicon Need help?  type /help `)
+m.reply( `Hello ${taguser} Am STAR-MD-V2 creatd by Excel Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(Xlicon|Team Xlicon)$/i
+handler.customPrefix = /^(Xcel|Excel)$/i
 handler.command = new RegExp
 
 export default handler
