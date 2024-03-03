dc6c93440cd687628640990c100fbc7851fe612c
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-03 22:10:15 +0100

    Update main-logomenu2.js

--- plugins/main-logomenu2.js
+++ plugins/main-logomenu2.js
@@ -42,7 +42,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let speed = neww - old
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './XLICON.jpg')
+//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './STAR.jpg')
 let user = global.db.data.users[who]
   
 let infobt = `
