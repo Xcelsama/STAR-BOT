abcf356ab40f5df8229085b136e536428c8accb8
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-03 11:57:01 +0100

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -20,12 +20,12 @@ let handler = async function (m, { conn, __dirname }) {
 📂 *ᏴϴͲ ΝᎪᎷᎬ:* _${repoData.name}_
 📝 *ᎠᎬՏᏟᎡᏆᏢͲᏆϴΝ:* _𝚂𝚃𝙰𝚁-𝙼𝙳-𝚅𝟸: ᴛʀᴀɴsғᴏʀᴍ ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ ᴇxᴘᴇʀɪᴇɴᴄᴄ ᴡɪᴛʜ sᴛᴀʀ-ᴍᴅ-ᴠ2! ᴛʜɪs ᴜʟᴛɪᴍᴀᴛᴇ ᴍᴇssᴀɢɪɴɢ ᴄᴏᴍᴘᴀɴɪᴏɴ ʙʀɪɴɢs ᴇxᴄɪᴛᴇᴍᴇɴᴛ ᴀɴᴅ ᴀ ᴘʟᴇᴛʜᴏʀᴀ ᴏғ ᴛʜʀɪʟʟɪɴɢ ғᴇᴀᴛᴜʀᴇs ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛs_
 👤 *ϴᏔΝᎬᎡ:* _EXCEL AMADI_
-⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_    
+⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_ 
+🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_   
 🔍 *ᎦᏟᎪΝ-ϘᎡ:*https://session.guruapi.tech/
 ⚔️ *ᘜᖇOᑌᑭ:* https://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs
 📡 *ᑭᑌᗷᒪIᑕ ᏀᖇOᑌᑭ:* https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK
-💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m
-🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_
+💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m 
 🌐 *ႮᎡᏞ:* ${repoData.html_url}
 🌠 *ΝϴᏔ ᎠᎬᏢᏞϴᎽ*:-https://dashboard.heroku.com/new?template=https://github.com/Xcelsama/STAR-MD-V2' 
 
