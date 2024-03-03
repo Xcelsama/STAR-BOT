dd7ceffd515d457c859028075bd73f960f3d79ff
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-03 21:57:11 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3", "Xlicon4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["STAR", "STAR1", "STAR2", "STAR3", "STAR4"])}.mp3`
 
-m.reply( `Hello ${taguser} Am STAR-MD-V2 creatd by Excel Need help?  type /help `)
+m.reply( `Hello ${taguser} Am STAR-MD-V2 created by Excel Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
