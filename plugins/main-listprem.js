15e4d33459c427d128bf0c21eda5d6b4c4be864c
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-05 20:59:00 +0100

    ՏͲᎪᎡ-ᎷᎠ-Ꮩ2

--- plugins/main-listprem.js
+++ plugins/main-listprem.js
@@ -1,7 +1,8 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid) 
-let teks = `▢ *PREMIUM USERS*\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
+let teks = `▢ *PREMIUM USERS OF STAR
+*\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 m.reply(teks, null, {mentions: conn.parseMention(teks)})
 
 }
