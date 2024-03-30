5fe1b301e5010c3222cf1da4c6f4d50a1419eb72
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-03-30 17:23:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,17 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['234704535241', 'EXCEL', true], 
-  ['2348021779696', 'EXCEL', true], 
-  ['234704503541', '⚡Excel', true],
+  ['919888280858', 'SYAM-owner, true], 
 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2341', '2348021779696', '2347045035241','233533763772'] 
-global.prems = ['2347045035241','233208179431','233533763772']
-global.allowed = ['2348021779696', '2347045035241']
+global.mods = ['919888280858', '2348021779696', '2347045035241','233533763772'] 
+global.prems = ['919888280858','233208179431','233533763772']
+global.allowed = ['2348021779696','919888280858', '2347045035241']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,21 +50,21 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'STAR-MD-V2'
+global.botname = 'BELUGA BOT😐'
 global.premium = 'true'
-global.packname = 'STAR-MD' 
-global.author = '@Excel' 
-global.menuvid = 'https://i.imgur.com/BPqmxsm.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp='▢✓ Follow My channel\nhttps://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs\n'
+global.packname = 'BELUGA BOT' 
+global.author = '@ig| _sks_bot_' 
+global.menuvid = 'https://i.imgur.com/HETrRgj.jpeg'
+global.igfg = '▢✓ Join my Group\nhttps://chat.whatsapp.com/DAMuVjxVMplGoEpaTyINMa\n' 
+global.dygp='▢✓ Join my Group\nhttps://chat.whatsapp.com/DAMuVjxVMplGoEpaTyINMa\n'
 global.fgsc = 'https://github.com/Xcelsama/STAR-MD-V2' 
-global.fgyt = 'https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK'
-global.fgpyp = 'https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK'
-global.fglog = 'STAR.jpg' 
+global.fgyt = 'https://chat.whatsapp.com/DAMuVjxVMplGoEpaTyINMa'
+global.fgpyp = 'https://chat.whatsapp.com/DAMuVjxVMplGoEpaTyINMa'
+global.fglog = 'BELUGA.jpg' 
 global.thumb = fs.readFileSync('./STAR.jpg')
 
 
-global.wait = '*🕣 _STAR IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _BELUGA IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '🔜'
 global.dmoji = '⚡'
 global.done = '☑️'
