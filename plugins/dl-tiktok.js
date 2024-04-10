ff368939674bacaf59134c0dd939a212906f6e32
Romek <149534184+Romeofaiz@users.noreply.github.com>
2024-04-11 04:27:27 +0530

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -10,7 +10,7 @@ if (!args[0] && !m.quoted) throw `Give the link of the video Tiktok or quote a t
  if (!args[0].match(/tiktok/gi)) throw `Verify that the link is from TikTok`;
  
  
-  let txt = 'ՏͲᎪᎡ-ᎷᎠ ͲͲ ᎠϴᏔΝᏞϴᎪᎠᎬᎡ';
+  let txt = '_ROMEK-XD TT DOWNLOADER_ ';
 
   try {
     const { author: { nickname }, video, description } = await tiktokdl(args[0]);
