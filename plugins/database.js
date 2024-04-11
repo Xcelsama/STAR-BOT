776214a79ec019db1151373fdd2cae8c89aa8b0a
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-04-11 01:20:28 +0100

    Update database.js

--- plugins/database.js
+++ plugins/database.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    m.reply(`*${totalreg} users using Bot*`)
+    m.reply(`*${totalreg} :-This are the total People using Bot*`)
 }
 handler.help = ['database', 'user']
 handler.tags = ['main']
