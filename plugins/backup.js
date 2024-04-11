697cedcd703b5d3e402a71b3d07f46e220f2d77c
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-04-12 00:53:29 +0100

    Update plugins

---
+++ plugins/backup.js
@@ -0,0 +1,55 @@
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn }) => {
+    conn.reply(m.chat, `
+┌─❖『 *_BACKUP_MENU_* 』❖
+┃❏*perv
+┃❏*handsome
+┃❏*hot
+┃❏*sexy
+┃❏*ugly
+┃❏*cute
+┃❏*playboy
+┃❏*playgirl
+┃❏*beautiful
+┃❏*lesbian
+┃❏*whore
+┃❏*motherfucker
+┃❏*sucker
+┃❏*horny
+┃❏*foolish
+┃❏*nibba
+┃❏*nibbi
+┃❏*bitch
+┃❏*waifu
+┃❏*crackhead
+┃❏*rascal
+┃❏*idiot
+┃❏*girlyboy
+┃❏*tomboy
+┃❏*gigachad
+┃❏*mf
+┃❏*introvert
+┃❏*extrovert
+┃❏*sigma
+┃❏*psycho
+┃❏*brainless
+┃❏*useless
+┃❏*singer
+┃❏*image
+┃❏*meme
+┃❏*quote
+╰─────────────────❖
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n')}
+
+`.trim(), null, {
+        mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
+    })
+}
+
+
+handler.help = ['backupmenu''bmenu']
+handler.tags = ['main']
+handler.command = ['backupmenu']
+
+export default handler
