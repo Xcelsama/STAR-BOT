6dc89aa8f634b674674658d972577ef031bdde6b
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-04-11 01:43:38 +0100

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -6,8 +6,8 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./Assets/ALIVE.mp3"
     let url = "https://github.com/Xcelsama"
-    let murl = "https://YouTube.com"
-    let img = "https://i.imgur.com/mQDfG3w.jpeg"
+    let murl = "https://github.com"
+    let img = "https://telegra.ph/file/f2fa9732e0e26bbb0bfb0.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -16,12 +16,12 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         mimetype: 'audio/mp4',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Excel",
-    
+        fileName: "Guru",
+
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "THE STAR-V2 IS ALIVE 🌟",
+          title: "🌪BOT IS ACTIVE🌪",
           body: "STAR-MD",
           thumbnailUrl: img,
           sourceUrl: 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m',
@@ -29,11 +29,11 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
           renderLargerThumbnail: true
           }}
       };
-    
+
       await conn.sendMessage(m.chat, doc, { quoted: con });
-    
+
     }
-    
+
     handler.help = ['alive']
     handler.tags = ['main']
     handler.command = /^(alive)$/i 
