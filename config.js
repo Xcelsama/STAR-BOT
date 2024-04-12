68471ae55e624f0ff2828c5eb8c6537f2b3979df
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-04-12 01:39:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['2347045035241', 'EXCEL', true], 
-  ['234802179696', 'EXCEL', true], 
-  ['07045035241', '⚡Excel', true],
-  ['2347045035241', 'Excel', true] ]
+  ['23470505241', 'EXCEL', true], 
+  ['234802696', 'EXCEL', true], 
+  ['070435241', '⚡Excel', true],
+  ['2347035241', 'Excel', true] ]
 
  //Number of owners
 
