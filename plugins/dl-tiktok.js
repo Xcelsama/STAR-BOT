0247c9d99187c8afbc317972e35de1099ca6c093
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-04-16 15:45:27 +0100

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -2,22 +2,22 @@ import { tiktokdl } from '@bochilteam/scraper';
 import fg from 'api-dylux';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  
+
  if (!args[0] && m.quoted && m.quoted.text) {
   args[0] = m.quoted.text;
 }
 if (!args[0] && !m.quoted) throw `Give the link of the video Tiktok or quote a tiktok link`;
  if (!args[0].match(/tiktok/gi)) throw `Verify that the link is from TikTok`;
- 
- 
-  let txt = 'ՏͲᎪᎡ ͲͲ ᎠϴᏔΝᏞϴᎪᎠᎬᎡ';
+
+
+  let txt = 'Here your Requested video';
 
   try {
     const { author: { nickname }, video, description } = await tiktokdl(args[0]);
     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
-    
+
     if (!url) throw global.error;
-    
+
     conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
   } catch (err) {
     try {
