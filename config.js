e54f1675144af58f7fbe30e44d4da53c1103e228
Uzairakra <110653042+Uzairakra@users.noreply.github.com>
2024-04-30 09:57:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = ['2347045035241', 'Excel', true]
+global.owner = ['923150513637', 'Excel', true]
  //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2347045035241'] 
-global.prems = ['2347045035241']
-global.allowed = ['2347045035241']
+global.mods = ['923150513637'] 
+global.prems = ['923150513637']
+global.allowed = ['923150513637']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -54,11 +54,11 @@ global.premium = 'true'
 global.packname = 'EX-BOTS_TEAM' 
 global.author = '@EXCEL' 
 global.menuvid = 'https://i.imgur.com/p7sdeTT.mp4'
-global.igfg = '▢✓ Follow My channel_ https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m\n' 
+global.igfg = '▢✓ Follow My channel_ https://whatsapp.com/channel/0029VaYi5xz5a243J3KvQB0Z\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
 global.fgsc = 'https://github.com/Xcelsama/STAR-MD-V2' 
-global.fgyt = 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m'
-global.fgpyp = 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m'
+global.fgyt = 'https://whatsapp.com/channel/0029VaYi5xz5a243J3KvQB0Z'
+global.fgpyp = 'https://whatsapp.com/channel/0029VaYi5xz5a243J3KvQB0Z'
 global.fglog = 'STAR.jpg' 
 global.thumb = fs.readFileSync('./STAR.jpg')
 
