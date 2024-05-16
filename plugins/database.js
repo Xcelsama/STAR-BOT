236fcb903cc24789223f91fe36c46cdb972f8e80
Romekxd <149534184+Romeofaiz@users.noreply.github.com>
2024-05-16 17:45:49 +0530

    Delete plugins directory

--- plugins/database.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    m.reply(`*${totalreg} :-This are the total People using Bot*`)
-}
-handler.help = ['database', 'user']
-handler.tags = ['main']
-handler.command = /^(database|db|user)$/i
-export default handler
