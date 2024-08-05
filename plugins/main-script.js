58910ed4e09f32f6b68822383fd3dcb846bee322
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-05 12:42:24 +0100

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -16,18 +16,16 @@ let handler = async function (m, { conn, __dirname }) {
 
       // Format the repository information with emojis
       const formattedInfo = `
-    ─────《 𝐒𝐓𝐀𝐑-𝐌𝐃-𝐕𝟐 》─────⊷
- 📂 *ᏴϴͲ ΝᎪᎷᎬ:* _${repoData.name}_
-📝 *ᎠᎬՏᏟᎡᏆᏢͲᏆϴΝ:* _Ꭺ ᏔᎻᎪͲՏᎪᏢᏢ ᏴϴͲ ᎡᎬᏟϴᎡᎠᎬᎠ ᏴᎽ ᎬХᏟᎬᏞ... ᏔᏆͲᎻ ᏞϴͲՏ ϴҒ ᎡᏆᏟᎻ ҒႮΝᏟͲᏆϴΝՏ.... 
-👤 *ᎠᎬᏙᎬᏞϴᏢᎬᎡ:* _EXCEL AMADI_
-⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_ 
-🗜*PAIR CODE:* https://replit.com/@Xcelsama/STAR-MD-V2-PAIR-CODE?s=app
-🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_ 
-⚔️ *ᘜᖇOᑌᑭ:* https://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs
-📡 *ᑭᑌᗷᒪIᑕ ᏀᖇOᑌᑭ:* https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK
-💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029VaBcXo4JJhzW9c1uVD2X 
-🌐 *ႮᎡᏞ:* ${repoData.html_url}
-🌠 *ΝϴᏔ ᎠᎬᏢᏞϴᎽ*:-https://dashboard.heroku.com/new?template=https://github.com/Xcelsama/STAR-V2' 
+    ────〘 𝐒𝐓𝐀𝐑-𝐌𝐃-𝐕𝟐 〙───⊷
+
+🤖 𝐁𝐎𝐓-𝐍𝐀𝐌𝐄: _${repoData.name}_
+📝 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐭𝐢𝐨𝐧:  A bot Multidevice Bot with    tons of functions
+👤 𝐃𝐄𝐕: _EXCEL AMADI_
+⭐ 𝐒𝐓𝐀𝐑𝐒: _${repoData.stargazers_count}_ 
+📲 𝐏𝐀𝐈𝐑 𝐂𝐎𝐃𝐄: https://replit.com/@Xcelsama/STAR-BOT-PAIRr?s=app
+🍴 𝐅𝐎𝐑𝐊𝐒: _${repoData.forks_count}_ 
+💻 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐂𝐇𝐀𝐍𝐍𝐄𝐋: https://whatsapp.com/channel/0029VaBcXo4JJhzW9c1uVD2X 
+🌐 𝐔𝐑𝐋: ${repoData.html_url}
 
  `.trim();
 
