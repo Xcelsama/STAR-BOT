2a15e68e8ad74a23d1d270133c096c1c2ac3b94e
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-12 09:14:26 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,7 +5,7 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["STAR", "STAR1", "STAR2", "STAR3", "STAR4"])}.mp3`
 
-m.reply( `*Hello ${taguser} Am STAR-MD-V2 created by Excel Do Need help?  type *help *`)
+m.reply( `Hello ${taguser} Am STAR-MD-V2 created by Excel Do Need help?  type ${used prefix}Help`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
