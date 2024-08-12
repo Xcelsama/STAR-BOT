d3b75b59b9d917c176cb8ed84b90819ede483b92
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-12 17:59:41 +0100

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -26,7 +26,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
         throw new Error('No valid JSON response from the first API')
       }
 
-      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/f6908fee9f896b7cb6e63.jpg', [['Script', `${usedPrefix}script`]], null, [['Fork Repo', `https://github.com/Xcelsama/STAR-MD-V2/fork`]], m)
+      await conn.sendButton(m.chat,result, author, 'https://ibb.co/ZxfDcW0', [['Script', `${usedPrefix}script`]], null, [['Fork Repo', `https://github.com/Xcelsama/STAR-MD-V2/fork`]], m)
       m.react(done)
     } catch (error) {
       console.error('Error from the first API:', error)
@@ -40,7 +40,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
       let data = await response.json()
       let result = data.completion
 
-     await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/f6908fee9f896b7cb6e63.jpg', [['Script', `.sc`]], null, [['Fork Repo', `https://github.com/Xcelsama/STAR-MD-V2/fork`]], m)
+     await conn.sendButton(m.chat,result, author, 'https://ibb.co/ZxfDcW0', [['Script', `.sc`]], null, [['Fork Repo', `https://github.com/Xcelsama/STAR-MD-V2/fork`]], m)
       m.react(done)
     }
   } catch (error) {
