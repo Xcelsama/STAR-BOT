b5cb873246720462323af331b6e5324e6a5f26dc
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-13 20:29:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,48 @@
-import express from 'express'
-import { createServer } from 'http'
-import path from 'path'
-import { Socket } from 'socket.io'
-import { toBuffer } from 'qrcode'
-import fetch from 'node-fetch'
+import express from 'express';
+import { createServer } from 'http';
+import path from 'path';
+import { toBuffer } from 'qrcode';
+import fetch from 'node-fetch';
 
-function connect(conn, PORT) {
-  let app = (global.app = express())
-  console.log(app)
-  let server = (global.server = createServer(app))
-  // app.use(express.static(path.join(__dirname, 'views')))
-  let _qr = 'invalid'
+function connect(conn, PORT, opts = {}) {
+  let app = (global.app = express());
+  let server = (global.server = createServer(app));
+  let _qr = 'invalid';
 
-  conn.ev.on('connection.update', function appQR({ qr }) {
-    if (qr) _qr = qr
-  })
+  conn.ev.on('connection.update', ({ qr }) => {
+    if (qr) _qr = qr;
+  });
 
   app.use(async (req, res) => {
-    res.setHeader('content-type', 'image/png')
-    res.end(await toBuffer(_qr))
-  })
-
-  // let io = new Socket(server)
-  // io.on('connection', socket => {
-  //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-  //     socket.on('disconnect', unpipeEmit)
-  // })
+    res.setHeader('content-type', 'image/png');
+    res.end(await toBuffer(_qr));
+  });
 
   server.listen(PORT, () => {
-    console.log('App listened on port', PORT)
-    if (opts['keepalive']) keepAlive()
-  })
-}
+    console.log('App listened on port', PORT);
+    if (opts.keepalive) keepAlive();
+  });
 
-function pipeEmit(event, event2, prefix = '') {
-  let old = event.emit
-  event.emit = function (event, ...args) {
-    old.emit(event, ...args)
-    event2.emit(prefix + event, ...args)
-  }
-  return {
-    unpipeEmit() {
-      event.emit = old
-    },
+  // Add a ping route to keep the server alive
+  app.get('/ping', (req, res) => {
+    res.send('Pong!');
+  });
+
+  // Set up a cron job to ping the server every 5 minutes
+  if (opts.keepalive) {
+    const cron = require('node-cron');
+    cron.schedule('*/5 * * * *', () => {
+      fetch(`http://localhost:${PORT}/ping`).catch(console.error);
+    });
   }
 }
 
 function keepAlive() {
-  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-  if (/(\/\/|\.)undefined\./.test(url)) return
-  setInterval(
-    () => {
-      fetch(url).catch(console.error)
-    },
-    5 * 1000 * 60
-  )
+  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;
+  if (/(\/\/|\.)undefined\./.test(url)) return;
+  setInterval(() => {
+    fetch(url).catch(console.error);
+  }, 5 * 1000 * 60);
 }
 
-export default connect
+export default connect;
