ffe63eb8b9c731ded12c34d904555076372b52d4
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-20 18:19:57 +0100

    Update dl-mega.js

--- plugins/dl-mega.js
+++ plugins/dl-mega.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
 }
 
 handler.help = ["mega"]
-handler.tags = ["downloader"]
+handler.tags = ["internet"]
 handler.command = /^(mega)$/i
 export default handler
 
