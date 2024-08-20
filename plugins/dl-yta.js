f052bae0e50714f77ead3c60ee02a9379882734f
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-20 18:27:33 +0100

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 };
 
 handler.help = ['ytmp3 <url>']
-handler.tags = ['downloader']
+handler.tags = ['internet']
 handler.command = ['ytmp3', 'yta'] 
 
 export default handler
