ce8d57c94fa1c51ed9c6242fde815176721ba5ce
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-08-22 10:45:45 +0100

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,7 +5,7 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɪɴɴᴏᴄᴇɴᴛ ᴍᴏʀᴇ ᴛʜᴀɴ ɢᴏᴏɢʟᴇ`, `ms.excel.amadi@gmail.com`, `🇧🇩 Bangladesh`, `🐙 https://github.com/Xcelsama `, `👑 ᴏᴡɴᴇʀ STAR-BOT `],
+    [`*%name*`, `${await conn.getName( *%name*')}`, `💌 Developer Bot `, `ɪɴɴᴏᴄᴇɴᴛ ᴍᴏʀᴇ ᴛʜᴀɴ ɢᴏᴏɢʟᴇ`, `ms.excel.amadi@gmail.com`, `🇳🇬 Ｎｉｇｅｒｉａ`, `🐙 https://github.com/Xcelsama `, `👑 ᴏᴡɴᴇʀ STAR-BOT `],
     [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `ɴᴏᴛʜɪɴɢ`, `🇳🇬 Nigeria `, `🐙 https://github.com/Xcelsama/STAR-BOT `, `🤖 STAR-BOT `]
   ], fkontak)
   await m.reply(`Listen baby @${m.sender.split('@')[0]}, That's My Owner Don't be over, Okay? 😃\nTry to be polite\nDon\t call him or you will be ignored`)
